72a1ef30dc56649d8d352c2f96ff1322a272cb78
Aidan <adonnelly759@qub.ac.uk>
2020-02-12 01:15:21 +0000

    Working chat and styled, just to style on main page now

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,13 +1,58 @@
+from django.contrib.auth import get_user_model
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
+from .models import Message
+
+User = get_user_model()
 
 class ChatConsumer(WebsocketConsumer):
+    def fetch_messages(self, data):
+        messages = Message.last_10_messages()
+        content = {
+            'command': 'messages',
+            'messages': self.messages_to_json(messages)
+        }
+        self.send_message(content)
+
+    def messages_to_json(self, messages):
+        result = []
+        for message in messages:
+            result.append(self.message_to_json(message))
+        return result
+
+    def message_to_json(self, message):
+        return {
+            'author': message.author.username,
+            'content': message.content,
+            'roomName': message.roomName,
+            'timestamp': str(message.timestamp)
+        }
+
+    def new_message(self, data):
+        author = data['from']
+        roomName = data['roomName']
+        author_user = User.objects.filter(username=author)[0]
+        message = Message.objects.create(
+            author=author_user, 
+            roomName=roomName,
+            content=data['message'])
+        content = {
+            'command': 'new_message',
+            'message': self.message_to_json(message)
+        }
+        return self.send_chat_message(content)
+
+
+    commands = {
+        'fetch_messages': fetch_messages,
+        'new_message': new_message
+    }
+
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-        # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
@@ -16,18 +61,16 @@ class ChatConsumer(WebsocketConsumer):
         self.accept()
 
     def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
-    # Receive message from WebSocket
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        data = json.loads(text_data)
+        self.commands[data['command']](self, data)
 
-        # Send message to room group
+    def send_chat_message(self, message):
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
@@ -36,11 +79,9 @@ class ChatConsumer(WebsocketConsumer):
             }
         )
 
-    # Receive message from room group
+    def send_message(self, message):
+        self.send(text_data=json.dumps(message))
+
     def chat_message(self, event):
         message = event['message']
-
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        self.send(text_data=json.dumps(message))
--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,12 @@
+from django.utils.safestring import mark_safe
 from django.shortcuts import render
+import json
 
 def index(request):
     return render(request, 'chat/index.html', {})
 
 def room(request, room_name):
     return render(request, 'chat/room.html', {
-        'room_name': room_name
+        'room_name': mark_safe(json.dumps(room_name)),
+        'username': mark_safe(json.dumps(request.user.username))
     })
